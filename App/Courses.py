53700d495f13f7acb378a3d81d6ece7ca4fd3b15
Kartik Kukreja <kukrejakartik010203@gmail.com>
2023-11-24 22:32:55 +0530

    first

---
+++ App/Courses.py
@@ -0,0 +1,102 @@
+ds_course = [['Machine Learning Crash Course by Google [Free]', 'https://developers.google.com/machine-learning/crash-course'],
+             ['Machine Learning A-Z by Udemy','https://www.udemy.com/course/machinelearning/'],
+             ['Machine Learning by Andrew NG','https://www.coursera.org/learn/machine-learning'],
+             ['Data Scientist Master Program of Simplilearn (IBM)','https://www.simplilearn.com/big-data-and-analytics/senior-data-scientist-masters-program-training'],
+             ['Data Science Foundations: Fundamentals by LinkedIn','https://www.linkedin.com/learning/data-science-foundations-fundamentals-5'],
+             ['Data Scientist with Python','https://www.datacamp.com/tracks/data-scientist-with-python'],
+             ['Programming for Data Science with Python','https://www.udacity.com/course/programming-for-data-science-nanodegree--nd104'],
+             ['Programming for Data Science with R','https://www.udacity.com/course/programming-for-data-science-nanodegree-with-R--nd118'],
+             ['Introduction to Data Science','https://www.udacity.com/course/introduction-to-data-science--cd0017'],
+             ['Intro to Machine Learning with TensorFlow','https://www.udacity.com/course/intro-to-machine-learning-with-tensorflow-nanodegree--nd230']]
+
+web_course = [['Django Crash course [Free]','https://youtu.be/e1IyzVyrLSU'],
+              ['Python and Django Full Stack Web Developer Bootcamp','https://www.udemy.com/course/python-and-django-full-stack-web-developer-bootcamp'],
+              ['React Crash Course [Free]','https://youtu.be/Dorf8i6lCuk'],
+              ['ReactJS Project Development Training','https://www.dotnettricks.com/training/masters-program/reactjs-certification-training'],
+              ['Full Stack Web Developer - MEAN Stack','https://www.simplilearn.com/full-stack-web-developer-mean-stack-certification-training'],
+              ['Node.js and Express.js [Free]','https://youtu.be/Oe421EPjeBE'],
+              ['Flask: Develop Web Applications in Python','https://www.educative.io/courses/flask-develop-web-applications-in-python'],
+              ['Full Stack Web Developer by Udacity','https://www.udacity.com/course/full-stack-web-developer-nanodegree--nd0044'],
+              ['Front End Web Developer by Udacity','https://www.udacity.com/course/front-end-web-developer-nanodegree--nd0011'],
+              ['Become a React Developer by Udacity','https://www.udacity.com/course/react-nanodegree--nd019']]
+
+android_course = [['Android Development for Beginners [Free]','https://youtu.be/fis26HvvDII'],
+                  ['Android App Development Specialization','https://www.coursera.org/specializations/android-app-development'],
+                  ['Associate Android Developer Certification','https://grow.google/androiddev/#?modal_active=none'],
+                  ['Become an Android Kotlin Developer by Udacity','https://www.udacity.com/course/android-kotlin-developer-nanodegree--nd940'],
+                  ['Android Basics by Google','https://www.udacity.com/course/android-basics-nanodegree-by-google--nd803'],
+                  ['The Complete Android Developer Course','https://www.udemy.com/course/complete-android-n-developer-course/'],
+                  ['Building an Android App with Architecture Components','https://www.linkedin.com/learning/building-an-android-app-with-architecture-components'],
+                  ['Android App Development Masterclass using Kotlin','https://www.udemy.com/course/android-oreo-kotlin-app-masterclass/'],
+                  ['Flutter & Dart - The Complete Flutter App Development Course','https://www.udemy.com/course/flutter-dart-the-complete-flutter-app-development-course/'],
+                  ['Flutter App Development Course [Free]','https://youtu.be/rZLR5olMR64']]
+
+ios_course = [['IOS App Development by LinkedIn','https://www.linkedin.com/learning/subscription/topics/ios'],
+              ['iOS & Swift - The Complete iOS App Development Bootcamp','https://www.udemy.com/course/ios-13-app-development-bootcamp/'],
+              ['Become an iOS Developer','https://www.udacity.com/course/ios-developer-nanodegree--nd003'],
+              ['iOS App Development with Swift Specialization','https://www.coursera.org/specializations/app-development'],
+              ['Mobile App Development with Swift','https://www.edx.org/professional-certificate/curtinx-mobile-app-development-with-swift'],
+              ['Swift Course by LinkedIn','https://www.linkedin.com/learning/subscription/topics/swift-2'],
+              ['Objective-C Crash Course for Swift Developers','https://www.udemy.com/course/objectivec/'],
+              ['Learn Swift by Codecademy','https://www.codecademy.com/learn/learn-swift'],
+              ['Swift Tutorial - Full Course for Beginners [Free]','https://youtu.be/comQ1-x2a1Q'],
+              ['Learn Swift Fast - [Free]','https://youtu.be/FcsY1YPBwzQ']]
+
+uiux_course = [['Google UX Design Professional Certificate','https://www.coursera.org/professional-certificates/google-ux-design'],
+               ['UI / UX Design Specialization','https://www.coursera.org/specializations/ui-ux-design'],
+               ['The Complete App Design Course - UX, UI and Design Thinking','https://www.udemy.com/course/the-complete-app-design-course-ux-and-ui-design/'],
+               ['UX & Web Design Master Course: Strategy, Design, Development','https://www.udemy.com/course/ux-web-design-master-course-strategy-design-development/'],
+               ['The Complete App Design Course - UX, UI and Design Thinking','https://www.udemy.com/course/the-complete-app-design-course-ux-and-ui-design/'],
+               ['DESIGN RULES: Principles + Practices for Great UI Design','https://www.udemy.com/course/design-rules/'],
+               ['Become a UX Designer by Udacity','https://www.udacity.com/course/ux-designer-nanodegree--nd578'],
+               ['Adobe XD Tutorial: User Experience Design Course [Free]','https://youtu.be/68w2VwalD5w'],
+               ['Adobe XD for Beginners [Free]','https://youtu.be/WEljsc2jorI'],
+               ['Adobe XD in Simple Way','https://learnux.io/course/adobe-xd']]
+
+resume_videos = [ 'https://youtu.be/Tt08KmFfIYQ','https://youtu.be/y8YH0Qbu5h4',
+                  'https://youtu.be/u75hUSShvnc','https://youtu.be/BYUy1yvjHxE',
+                  'https://youtu.be/KFaugkGVeNQ','https://youtu.be/3agP4x8LYFM',
+                  'https://youtu.be/GyjzOKdaioU','https://youtu.be/17YZBH_qtmg',
+                  'https://youtu.be/C7pNLIq3kOI','https://youtu.be/xpaz7nrNmXA',
+                  'https://youtu.be/aKjsy-b00QM','https://youtu.be/ciIkiWwZnlc']
+
+interview_videos = ['https://youtu.be/HG68Ymazo18','https://youtu.be/BOvAAoxM4vg',
+                    'https://youtu.be/KukmClH1KoA','https://youtu.be/7_aAicmPB3A',
+                    'https://youtu.be/1mHjMNZZvFo','https://youtu.be/WfdtKbAJOmE',
+                    'https://youtu.be/IBjM-F56qS0','https://youtu.be/IBjM-F56qS0',
+                    'https://youtu.be/4tYoVx0QoN0','https://youtu.be/Ge0Udbws1kc',
+                    'https://youtu.be/thkuu_FWFD8','https://youtu.be/e0E6-dRPcJA',
+                    'https://youtu.be/htT1bhFSNxo','https://youtu.be/TZ3C_syg9Ow']
+
+web_jobs=[['Angular Developer - FreightCom','https://in.indeed.com/q-web-developer-jobs.html?vjk=cc96a2f6d39a539c&advn=3910742313157298'],
+['Backend Software Developer - Think Huge Ltd.','https://in.indeed.com/q-web-developer-jobs.html?vjk=a7d36bf719cf3408&advn=4255150266785893'], 
+['Frontend Developer Intern - MyITROnline Global Services','https://in.indeed.com/q-web-developer-jobs.html?vjk=7ffc50866e2318b2'], 
+['Frontend Developer - Codeblocks','https://in.indeed.com/q-web-developer-jobs.html?vjk=d253e6820f26cdd3'], 
+['Jr. PHP Developer','https://in.indeed.com/q-web-developer-jobs.html?vjk=76c73ca12acb3663&advn=483845950800984']
+]
+
+android_jobs=[
+    ['React Native Developer - Gleantap','https://in.indeed.com/q-android-developer-jobs.html?vjk=661d9dee148c4405&advn=8562372038498564'],
+    ['Android Developer - 91Sqaurefeet','https://in.indeed.com/q-android-developer-jobs.html?vjk=03a2accf2172d7b0'],
+    ['Android Application Developer - Bijson Pvt Ltd.','https://in.indeed.com/q-android-developer-jobs.html?vjk=5c33df8ad3488b71'],
+    ['Android Developer Intern - IT Services Company','https://in.indeed.com/q-android-developer-jobs.html?vjk=49bc9bc9b817bce5'],
+    ['SDE-Android Developer - Unify Technologies','https://in.indeed.com/q-android-developer-jobs.html?vjk=40634f21302e0eab']
+]
+
+uiux_jobs = [['UI/UX Designer - Overseas Group Inc.', 'https://in.indeed.com/jobs?q=ui+ux+designer&l=India&from=searchOnHP&vjk=34d9b8fa39df55a4'], 
+                ['UI/UX Designer - BTOSC Infotech', 'https://in.indeed.com/jobs?q=ui+ux+designer&l=India&from=searchOnHP&vjk=34beb76ec8d19bc0'], 
+                ['Senior UX/UI Designer - Terra Motors', 'https://in.indeed.com/jobs?q=ui+ux+designer&l=India&from=searchOnHP&vjk=36824bd56a2801a7'],
+                ['UI/UX Designer - Nioclass' , 'https://in.indeed.com/jobs?q=ui+ux+designer&l=India&from=searchOnHP&vjk=74db42ba8f0593f9'],
+                ['UX Designer - CIT', 'https://in.indeed.com/jobs?q=ui+ux+designer&l=India&from=searchOnHP&vjk=49348f8cfa989bb1']]
+
+ds_jobs = [['Data Analyst - MyWays.ai', 'https://in.indeed.com/jobs?q=data+science&l=India&vjk=50b7a696d95130ec'],
+           ['Data Scientist - WhizHack Technologies', 'https://in.indeed.com/jobs?q=data+science&l=India&vjk=1715737956f9cb81'],
+           ['Data Scientist: Advanced Analytics - IBM', 'https://in.indeed.com/jobs?q=data+science&l=India&start=10&pp=gQAPAAAAAAAAAAAAAAACGDi68wAsAQACEGXJvsdjDgeeIhD-8DxXhoKKJKJeYLhg_QRG8crNi0PwzC1Q2BE0f-QAAA&vjk=fa6c5652379a74a6'],
+           ['Data Engineer - TMI Group', 'https://in.indeed.com/jobs?q=data+science&l=India&start=10&pp=gQAPAAAAAAAAAAAAAAACGDi68wAsAQACEGXJvsdjDgeeIhD-8DxXhoKKJKJeYLhg_QRG8crNi0PwzC1Q2BE0f-QAAA&vjk=b9d371546fad5a49'],
+           ['Data Engineer - DeltaCubes', 'https://in.indeed.com/jobs?q=data+science&l=India&start=10&pp=gQAPAAAAAAAAAAAAAAACGDi68wAsAQACEGXJvsdjDgeeIhD-8DxXhoKKJKJeYLhg_QRG8crNi0PwzC1Q2BE0f-QAAA&vjk=8e07c514f6a6675e']]
+
+ios_jobs = [['IOS Engineer - BigShyft', 'https://in.indeed.com/jobs?q=IOS&l=India&vjk=d80f159dc69f2396'],
+            ['React Native Developer - Gleantap', 'https://in.indeed.com/jobs?q=IOS&l=India&vjk=661d9dee148c4405&advn=8562372038498564'],
+            ['Jr IOS Developer - MVK Staffing Services', 'https://in.indeed.com/jobs?q=IOS&l=India&vjk=c3a1545d5063c40b'],
+            ['Senior IOS Developer - Soft Prodigy System Solutions', 'https://in.indeed.com/jobs?q=IOS&l=India&start=20&pp=gQAeAAAAAAAAAAAAAAACGDflywBAAQEBCgdUZmY2yaicYOx7Pou4qarBwutFGn7ywyxnibToxtuB-Uzq31SMXQxdJIQtdAZ3Z5B0BCzP6yKps9WqaAAA&vjk=05ac1f3f4797e31d'],
+            ['React Native Developer - Mobile Programming India', 'https://in.indeed.com/jobs?q=IOS&l=India&start=20&pp=gQAeAAAAAAAAAAAAAAACGDflywBAAQEBCgdUZmY2yaicYOx7Pou4qarBwutFGn7ywyxnibToxtuB-Uzq31SMXQxdJIQtdAZ3Z5B0BCzP6yKps9WqaAAA&vjk=3aec0fc7a1047e2b']]
